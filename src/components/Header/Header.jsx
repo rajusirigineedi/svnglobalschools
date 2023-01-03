6852535e0ff08dc8a157b72554ef10a8d2bf29e0
Sirigineedi Hemanth Pallapa Raju <sirigineedi@geekyants.com>
2023-01-04 00:52:46 +0530

    🛠 Add first section in home

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,20 @@
-import React from "react"
+import React from "react";
 import classes from "./header.module.css";
 
 export const Header = () => {
-    return <div className={classes["header"]}>
-        Header
+  return (
+    <div className={classes["header"]}>
+      <div>Srivani Global Schools</div>
+      <div>
+        <ul>
+          <li>Home</li>
+          <li>Academics</li>
+          <li>About</li>
+          <li>Admissions</li>
+          <li>Events</li>
+          <li>Contact</li>
+        </ul>
+      </div>
     </div>
-}
+  );
+};
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,60 @@
-import { Box } from "@mui/material";
-import "../styles/home.css";
+import { Button, Col, Row, Space, Typography } from "antd";
+import classes from "../styles/home.module.css";
 
+const { Title } = Typography;
 export const Home = () => {
   return (
-    <>
-      {/* Dont pick this page right now we'll think about it later */}
-      <center><h1>Today we are going to start this project and rightnow we are on design planning, so please keep an eye on this page.
-      We will be rightback!!!</h1></center>
-    </>
+    <div>
+      <section className={classes["intro-container"]}>
+        <h1 className={classes["school-title"]}>Srivani Global Schools</h1>
+      </section>
+      <section className={classes["caurosel-container"]}>
+        <Row style={{ height: "100%" }}>
+          <Col
+            xs={24}
+            sm={24}
+            md={16}
+            lg={16}
+            xl={16}
+            className={classes["important-news"]}
+          >
+            <Space direction="vertical">
+              <Space>
+                <span className={classes["blockquote-big"]}>"</span>
+                <Title level={3} style={{ color: "white" }}>
+                  Empowering students to reach their full potential Inspiring
+                  minds, shaping the future Where education meets opportunity
+                  Nurturing creativity and critical thinking
+                </Title>
+              </Space>
+              <Title level={5} style={{ color: "rgb(139, 139, 139)" }}>
+                Students to reach their full potential Inspiring minds, shaping
+                the future Where education meets opportunity Nurturing
+                creativity and critical thinking
+              </Title>
+            </Space>
+          </Col>
+          <Col
+            xs={24}
+            sm={24}
+            md={8}
+            lg={8}
+            xl={8}
+            className={classes["application-notifications"]}
+          >
+            <Space direction="vertical">
+              <Title level={3}>
+                Applications for the academic year{" "}
+                <span style={{ fontWeight: "900", fontSize: "3.6rem" }}>
+                  2022-2023{" "}
+                </span>{" "}
+                are open ...
+              </Title>
+              <Button className={classes["outline-button"]}>Contact Us</Button>
+            </Space>
+          </Col>
+        </Row>
+      </section>
+    </div>
   );
 };
