15405b1b97ec923365765e3b642610de03d32f9d
Bolisetti Durga Charan Naidu <charannbolisetti@gmail.com>
2023-01-13 23:17:36 +0530

    updated the code for routing

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,20 +1,33 @@
 import React from "react";
 import classes from "./header.module.css";
+import { Link } from "react-router-dom";
 
 export const Header = () => {
   return (
     <div className={classes["header"]}>
-      <div>Srivani Global Schools</div>
-      <div>
-        <ul>
-          <li>Home</li>
-          <li>Academics</li>
-          <li>About</li>
-          <li>Admissions</li>
-          <li>Events</li>
-          <li>Contact</li>
-        </ul>
+        <div>Srivani Global Schools</div>
+        <div>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/academics">Academics</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/admissions">Admissions</Link>
+            </li>
+            <li>
+              <Link to="/events">Events</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+          </ul>
+        </div>
       </div>
-    </div>
   );
 };
