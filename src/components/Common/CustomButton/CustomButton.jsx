e5b4bdfa001b35397f8815b960cf05d0c6fe4d41
ravi <mooninthedarknight1999@gmail.com>
2023-02-04 09:26:00 +0530

    SGS-10-Custom-reusable-button

---
+++ src/components/Common/CustomButton/CustomButton.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import classes from './CustomButton.module.css';
+const CustomButton = (props) => {
+    return (
+        <div>
+            <button
+                style={{
+                    color: props.styles.color,
+                    backgroundColor: props.styles.backgroundColor,
+                    height: props.styles.height || "32px",
+                    width: props.styles.width || "6rem",
+                    border: `1px solid ${props.styles.border}` || "",
+                    borderRadius: props.styles.borderRadius || "6px",
+                }}
+                onClick={props.onClick}
+            >
+                {props.children}</button>
+        </div>
+    );
+};
+
+export default CustomButton;
