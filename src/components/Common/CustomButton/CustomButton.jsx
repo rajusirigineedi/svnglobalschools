41698d9825e1c7d83b66831d73532e02c6a7245d
ravi <mooninthedarknight1999@gmail.com>
2023-02-04 09:39:08 +0530

    SGS-10-Custom-reusable-button

--- src/components/Common/CustomButton/CustomButton.jsx
+++ src/components/Common/CustomButton/CustomButton.jsx
@@ -8,7 +8,7 @@ const CustomButton = (props) => {
                     color: props.styles.color,
                     backgroundColor: props.styles.backgroundColor,
                     height: props.styles.height || "32px",
-                    width: props.styles.width || "6rem",
+                    width: props.styles.width || "100",
                     border: `1px solid ${props.styles.border}` || "",
                     borderRadius: props.styles.borderRadius || "6px",
                 }}
